5944049d02d8a28eb8edfae5d81cbe6f58acd1af
Dan Gomme <daniel.gomme@yahoo.co.uk>
2019-06-12 23:23:25 +0100

    get rid of printing out debug messages in KMeans

--- src/utils/Clustering/KMeansStateClusterer.java
+++ src/utils/Clustering/KMeansStateClusterer.java
@@ -163,9 +163,6 @@ public class KMeansStateClusterer implements Clusterer {
           closestMeanIndex = j;
         }
       }
-      if(closestMeanIndex == -1) {
-        System.out.println("Okay, so no closest mean index, possibly because mean size was 0? Skipping?");
-      }
       result.get(closestMeanIndex).add(i);
     }
     return result;
